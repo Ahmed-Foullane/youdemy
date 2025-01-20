253af3a38a0e51885ae8d4f3c03998ce15e66bf6
Ahmed-Foullane <ahmedfoullane@gmail.com>
2025-01-20 14:22:07 +0100

    create components for teacher

---
+++ views/components/teacher/sidbare.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <div class="side-bar">
+   <div id="close-btn">
+      <i class="fas fa-times"></i>
+   </div>
+   
+   <a class="btn" href="controllers/auth.php?logout=1" style="margin-left: 20px;">Logout</a>
+
+    <div class="profile">
+         
+         <h3 class="name">shaikh anas</h3>
+         <p class="role">teacher</p>
+        
+      </div>
+
+   <nav class="navbar">
+      <a href="/teacher-courses"><i class="fas fa-graduation-cap"></i><span>my courses</span></a>
+      <a href="/teacher-statistics"><i class="fa-solid fa-chart-simple"></i><span>statistics</span></a>
+      <a href="contact"><i class="fas fa-headset"></i><span>contact us</span></a>
+   </nav>
+
+</div>
+</body>
+</html>
+
+
